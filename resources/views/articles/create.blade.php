a81686c7702fa99d956c399534537e193aa5327e
Alexandre Tobia <alexandre.tobia@gmail.com>
2016-03-09 12:47:02 +0100

    Add partials for post

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,23 +2,8 @@
 
 @section('content')
     <h1>Ajouter un article</h1>
-    {!! Form::open(['route' => 'articles.store', 'method' => 'POST']) !!}
-        <div class="form-group">
-            {!! Form::select('user_id', $users, null, ['class' => 'form-control']) !!}
-        </div>
 
-        <div class="form-group">
-            {!! Form::text('title', null, [
-                'class' => 'form-control',
-                'placeholder' => 'Nom de l\'article'
-            ]) !!}
-        </div>
+    @include('partials.articles.form', ['action' => 'create'])
+    @include('partials.articles.errors')
 
-        <div class="form-group">
-            {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
-        </div>
-
-        {!! Form::submit('Envoyer', ['class' => 'btn btn-block']) !!}
-
-    {!! Form::close() !!}
 @endsection
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -2,23 +2,8 @@
 
 @section('content')
     <h1>Edit de l'article n'° {{ $post->id }}</h1>
-    {!! Form::open(['route' => ['articles.update', $post->id], 'method' => 'PUT']) !!}
-        <div class="form-group">
-            {!! Form::select('user_id', $users, $post->user_id, ['class' => 'form-control']) !!}
-        </div>
 
-        <div class="form-group">
-            {!! Form::text('title', $post->title, [
-                'class' => 'form-control',
-                'placeholder' => 'Nom de l\'article'
-            ]) !!}
-        </div>
+    @include('partials.articles.form', ['action' => 'edit'])
+    @include('partials.articles.errors')
 
-        <div class="form-group">
-            {!! Form::textarea('content', $post->content, ['class' => 'form-control']) !!}
-        </div>
-
-        {!! Form::submit('Envoyer', ['class' => 'btn btn-block']) !!}
-
-    {!! Form::close() !!}
 @endsection
